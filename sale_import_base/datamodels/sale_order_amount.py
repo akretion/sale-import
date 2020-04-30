e4d9d926f5d17dbbae205910ba58b4d82b20f6e1
kevinkhao <kevin.khao@akretion.com.br>
2020-04-30 16:44:48 +0200

    WIP

---
+++ sale_import_base/datamodels/sale_order_amount.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Akretion
+from odoo.addons.datamodel import fields
+from odoo.addons.datamodel.datamodels.base import BaseDatamodel
+
+
+class SaleOrderAmountDatamodel(BaseDatamodel):
+    _name = "sale.order.amount"
+
+    amount_tax = fields.Decimal(required=True)
+    amount_untaxed = fields.Decimal(required=True)
+    amount_total = fields.Decimal(required=True)
