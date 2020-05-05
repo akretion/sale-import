ab0e12e93f28ca98f164e04cff6ec78b2f8acf17
kevinkhao <kevin.khao@akretion.com.br>
2020-05-05 17:01:11 +0200

    WIP

--- sale_channel_partner_binding/models/sale_order.py
+++ sale_channel_partner_binding/models/sale_order.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    partner_binding_ids = fields.One2many(
-        "res.partner.binding", "sale_order_id", string="Partner bindings"
+    partner_binding_id = fields.Many2one(
+        "res.partner.binding", string="Partner binding", ondelete="set null"
     )
