83b6d2ca8ccafc0752da891e8d444062b781eb22
kevinkhao <kevin.khao@akretion.com.br>
2020-05-06 15:16:43 +0200

    [IMP] Modifications according to review: simplifications, removed sale_order_ids from partner bindings, removed copyrights, partner bindings writable, discount key for carrier optional, merge sale_import_payment into sale_import_base

--- sale_channel_partner_binding/models/partner_binding.py
+++ sale_channel_partner_binding/models/partner_binding.py
@@ -1,4 +1,3 @@
-# Copyright 2020 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -8,9 +7,6 @@ class SaleChannelPartnerBinding(models.Model):
     _name = "res.partner.binding"
     _description = "Sale Channel Partner Binding"
 
-    sale_order_ids = fields.One2many(
-        "sale.order", "partner_binding_id", string="Sale orders"
-    )
     sale_channel_id = fields.Many2one(
         "sale.channel", "Sale Channel", required=True, ondelete="cascade"
     )
--- sale_channel_partner_binding/models/sale_order.py
+++ sale_channel_partner_binding/models/sale_order.py
@@ -1,4 +1,3 @@
-# Copyright 2020 Akretion
 from odoo import fields, models
 
 
