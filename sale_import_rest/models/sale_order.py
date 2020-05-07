8f0119c17edd4fed4b0e8b8c9d4cad2434b5678f
kevinkhao <kevin.khao@akretion.com.br>
2020-05-07 14:55:19 +0200

    WIP

--- sale_import_rest/models/sale_order.py
+++ sale_import_rest/models/sale_order.py
@@ -4,19 +4,3 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    def _si_get_partner(self, so_vals):
-        sale_channel_id = self.env.context.get("sale_channel")
-        external_id = so_vals["address_customer"].get("external_id")
-        binding = self.env["res.partner.binding"].search(
-            [
-                ("partner_id", "=", external_id.id),
-                ("sale_channel_id", "=", sale_channel_id.id),
-            ]
-        )
-        if binding:
-            return binding.partner_id
-        if sale_channel_id.allow_match_on_email:
-            return super()._si_get_partner(so_vals)
-        else:
-            raise ValidationError(_("No customer found"))
