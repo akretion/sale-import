7fec888a15360887cfb4e1c09593bdba4a6f2f8e
kevinkhao <kevin.khao@akretion.com.br>
2020-05-11 07:12:52 +0200

    BIG WIP

--- sale_import_delivery_carrier/tests/test_sale_order_import_carrier.py
+++ sale_import_delivery_carrier/tests/test_sale_order_import_carrier.py
@@ -1,5 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+from copy import deepcopy
+
 from odoo.tools import float_compare
 
 from odoo.addons.sale_import_base.tests.common_sale_order_import import SaleImportCase
@@ -13,15 +15,16 @@ class TestSaleOrderImport(SaleImportCase):
             "price_unit": 10.0,
             "discount": 0.0,
         }
-        json_import = self.sale_order_example_vals
-        self.sale_order = self.env["sale.order"].process_json_import(json_import)
 
     def test_delivery_carrier_charges_applied(self):
-        delivery_line = self.sale_order.order_line.filtered(lambda r: r.is_delivery)
+        json_import = deepcopy(self.sale_order_example_vals)
+        sale_order = self.env["sale.order"].process_json_import(json_import)
+        delivery_line = sale_order.order_line.filtered(lambda r: r.is_delivery)
         self.assertTrue(delivery_line)
         delivery_amount = delivery_line.price_total
         expected_delivery_amount = 10.0
         equal_delivery = float_compare(
             delivery_amount, expected_delivery_amount, precision_digits=2
         )
+
         self.assertEqual(equal_delivery, 0)
