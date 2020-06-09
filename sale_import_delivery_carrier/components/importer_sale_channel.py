5a897ae29bffb48e9c03f28912bd811c458cce62
KevinKhao <kevinkhao@gmail.com>
2020-06-09 19:38:36 +0200

    [IMP] More cleanup/feedback improvements

--- sale_import_delivery_carrier/components/importer_sale_channel.py
+++ sale_import_delivery_carrier/components/importer_sale_channel.py
@@ -6,19 +6,22 @@ from odoo.addons.component.core import Component
 class ImporterSaleChannel(Component):
     _inherit = "importer.sale.channel"
 
-    def _prepare_vals(self, data):
-        vals = super()._prepare_vals(data)
-        return self._add_delivery_line(vals, data)
+    def _prepare_sale_lines(self, data):
+        vals = super()._prepare_sale_lines(data)
+        delivery_line_command = (
+            0,
+            0,
+            self._prepare_delivery_line(vals["partner_id"], data),
+        )
+        return vals + delivery_line_command
 
-    def _add_delivery_line(self, vals, data):
+    def _prepare_delivery_line(self, partner_id, data):
         if not data.get("delivery_carrier"):
-            return vals
+            return []
         delivery_carrier = self.env["delivery.carrier"].search(
             [("name", "=", data["delivery_carrier"]["name"])]
         )
-        price = data["delivery_carrier"]["price_unit"]
-        discount = data["delivery_carrier"]["discount"]
-        partner = self.env["res.partner"].browse(vals["partner_id"])
+        partner = self.env["res.partner"].browse(partner_id)
         carrier_with_partner_lang = delivery_carrier.with_context(lang=partner.lang)
         if carrier_with_partner_lang.product_id.description_sale:
             description = "{}: {}".format(
@@ -32,9 +35,8 @@ class ImporterSaleChannel(Component):
             "product_uom_qty": 1,
             "product_uom": delivery_carrier.product_id.uom_id.id,
             "product_id": delivery_carrier.product_id.id,
-            "price_unit": price,
-            "discount": discount,
+            "price_unit": data["delivery_carrier"]["price_unit"],
+            "discount": data["delivery_carrier"]["discount"],
             "is_delivery": True,
         }
-        vals["order_line"].append((0, 0, new_line_vals))
-        return vals
+        return new_line_vals
