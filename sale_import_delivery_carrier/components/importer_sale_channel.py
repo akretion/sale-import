72c1d2ed4522295d4915d31bc43412e55b7c097e
Kevin Khao <kevinkhao@gmail.com>
2020-08-24 15:02:52 +0200

    [IMP] Modifications related to changing "required" attribute on datamodels: includes SO generation, tests, components, demo data Add tests for cases where data content is full/minimal/mixed Fix state_code: search also according to country_id Fix state_code, country_code validation and search

--- sale_import_delivery_carrier/components/importer_sale_channel.py
+++ sale_import_delivery_carrier/components/importer_sale_channel.py
@@ -1,5 +1,7 @@
 #  Copyright (c) Akretion 2020
 #  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+from odoo.tools import float_compare
+
 from odoo.addons.component.core import Component
 
 
@@ -19,10 +21,15 @@ class ImporterSaleChannel(Component):
     def _prepare_sale_line_vals(self, data, sale_order):
         vals = super()._prepare_sale_line_vals(data, sale_order)
         delivery_line = self._prepare_delivery_line(data, sale_order)
-        return vals + [delivery_line]
+        if delivery_line:
+            vals += [delivery_line]
+        return vals
 
     def _prepare_delivery_line(self, data, sale_order):
-        if not data.get("delivery_carrier"):
+        precision = self.env["decimal.precision"].precision_get("Product Price")
+        price = float(data["delivery_carrier"]["price_unit"])
+        no_delivery_charges = float_compare(price, 0.0, precision_digits=precision)
+        if not data.get("delivery_carrier") or no_delivery_charges == 0:
             return
         delivery_carrier = self.env["delivery.carrier"].search(
             [("name", "=", data["delivery_carrier"]["name"])]
@@ -44,8 +51,8 @@ class ImporterSaleChannel(Component):
             "product_uom_qty": 1,
             "product_uom": delivery_carrier.product_id.uom_id.id,
             "product_id": delivery_carrier.product_id.id,
-            "price_unit": data["delivery_carrier"]["price_unit"],
-            "discount": data["delivery_carrier"]["discount"],
+            "price_unit": price,
+            "discount": data["delivery_carrier"].get("discount"),
             "is_delivery": True,
             "order_id": sale_order.id,
         }
