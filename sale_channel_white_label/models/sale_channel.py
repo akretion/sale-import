a4d5815ef93603326a01ea578c222bbed3c4f728
Florian da Costa <florian.dacosta@akretion.com>
2025-02-07 17:01:17 +0100

    [IMP] Put partner_id in sale_channel_white_label It has been removed from sale_channel (in OCA repo)

--- sale_channel_white_label/models/sale_channel.py
+++ sale_channel_white_label/models/sale_channel.py
@@ -6,6 +6,8 @@ from odoo import fields, models
 class SaleChannel(models.Model):
     _inherit = "sale.channel"
 
+    # should it be renamed like owner_id or something ? To be less generic
+    partner_id = fields.Many2one("res.partner")
     is_white_label = fields.Boolean(
         help="Check this box if this channel comes from a white label partner"
     )
